778b13fe1e8af1dafcc926ea11c0bb1df1452d73
Scott Steadman <scott.steadman@geni.com>
2020-03-24 13:00:14 -0700

    refactor CarrotTest
      I find this way of writing tests is a bit more readable
      than setting instance variables in setup and referencing
      them in each test.

--- test/carrot_test.rb
+++ test/carrot_test.rb
@@ -2,25 +2,27 @@ require 'test_helper'
 
 class CarrotTest < TestHelper
 
-  TEST_QUEUE = '_carrot_test'
-
-  def setup
-    @carrot = Carrot.new
-    @q      = @carrot.queue(TEST_QUEUE)
-    @q.purge
-  end
-
   test "large messages" do
     msg = 'a' * 1024 * 1024
-    @q.publish(msg)
-    assert_equal msg, @q.pop
+    queue.publish(msg)
+    assert_equal msg, queue.pop
   end
 
   test "reset" do
-    count = @q.message_count
-    @q.publish('test')
-    @carrot.reset
-    assert_equal count + 1, @q.message_count
+    count = queue.message_count
+    queue.publish('test')
+    carrot.reset
+    assert_equal count + 1, queue.message_count
+  end
+
+private
+
+  def carrot
+    @carrot ||= Carrot.new
+  end
+
+  def queue(name='_carrot_test')
+    @queue ||= carrot.queue(name)
   end
 
 end # context 'test queue'
